036e81c9e4c8a5791600caa42f271255aa91f8a3
Chris <chris.kernaghan@gmail.com>
2020-12-21 22:30:39 +0000

    Amended the code

--- create-vm.sh
+++ create-vm.sh
@@ -1,3 +1,4 @@
 gcloud compute instances create live-stream-test \
- --image debian-10-buster-v20201216 | --image-family debian-10 \
- --image-project debian-cloud 
+ --image debian-10-buster-v20201216 \
+ --image-project debian-cloud \
+ --zone=us-central1-a
