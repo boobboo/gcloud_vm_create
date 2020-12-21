b41d84d08a5bcc6f6aeae505c4e3f18b19ede19d
Chris <chris.kernaghan@gmail.com>
2020-12-21 22:22:52 +0000

    Instances not Instance

--- create-vm.sh
+++ create-vm.sh
@@ -1,3 +1,3 @@
-gcloud compute instance create live-stream-test \
+gcloud compute instances create live-stream-test \
  --image debian-10-buster-v20201216 | --image-family debian-10 \
  --image-project debian-cloud 
